f221af733ce4ccf1db3d1c8e0ae36fda7fd8f5c5
Robeir George <robier_samir@hotmail.co.uk>
2025-06-07 03:02:36 +0300

    Fix

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,8 +4,8 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    <a href="{{ route('dashboard') }}" class="text-xl font-bold text-gray-800">
+                        إعداد خدام 2025
                     </a>
                 </div>
 
