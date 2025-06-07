5143a6c14b5f102c7c0f464aa7b5609e2514f6d6
Robeir George <robier_samir@hotmail.co.uk>
2025-06-07 03:01:10 +0300

    Fix navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,17 +1,17 @@
-<nav class="bg-white shadow-lg">
+<nav class="bg-white border-b border-gray-100">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}" class="text-xl font-bold text-indigo-600">
-                        إعداد خدام 2025
+                <div class="shrink-0 flex items-center">
+                    <a href="{{ route('dashboard') }}">
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
-                <div class="hidden sm:ml-6 sm:flex sm:space-x-8 sm:space-x-reverse">
-                    @auth
+                @auth
+                    <!-- Navigation Links -->
+                    <div class="hidden sm:ml-6 sm:flex sm:space-x-8 sm:space-x-reverse">
                         <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                             الرئيسية
                         </x-nav-link>
@@ -27,13 +27,13 @@
                         <x-nav-link :href="route('sessions.index')" :active="request()->routeIs('sessions.*')">
                             المحاضرات
                         </x-nav-link>
-                    @endauth
-                </div>
+                    </div>
+                @endauth
             </div>
 
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:ml-6 sm:flex sm:items-center">
-                @auth
+            @auth
+                <!-- Settings Dropdown -->
+                <div class="hidden sm:ml-6 sm:flex sm:items-center">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
@@ -63,15 +63,15 @@
                             </form>
                         </x-slot>
                     </x-dropdown>
-                @endauth
-            </div>
+                </div>
+            @endauth
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = !open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': !open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
@@ -79,35 +79,50 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': !open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <a href="{{ route('dashboard') }}" class="{{ request()->routeIs('dashboard') ? 'bg-indigo-50 border-indigo-500 text-indigo-700' : 'border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700' }} block pr-3 py-2 border-r-4 text-base font-medium">
-                الرئيسية
-            </a>
-            <a href="{{ route('attendance.all') }}" class="{{ request()->routeIs('attendance.*') ? 'bg-indigo-50 border-indigo-500 text-indigo-700' : 'border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700' }} block pr-3 py-2 border-r-4 text-base font-medium">
-                الحضور
-            </a>
-            <a href="{{ route('sessions.index') }}" class="{{ request()->routeIs('sessions.*') ? 'bg-indigo-50 border-indigo-500 text-indigo-700' : 'border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700' }} block pr-3 py-2 border-r-4 text-base font-medium">
-                المحاضرات
-            </a>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->first_name . ' ' . Auth::user()->second_name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+        @auth
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    الرئيسية
+                </x-responsive-nav-link>
+                @if(auth()->user()->role === 'Admin' || auth()->user()->role === 'Instructor')
+                    <x-responsive-nav-link :href="route('attendance.all')" :active="request()->routeIs('attendance.*')">
+                        الحضور
+                    </x-responsive-nav-link>
+                @else
+                    <x-responsive-nav-link :href="route('attendance.my')" :active="request()->routeIs('attendance.my')">
+                        سجل الحضور
+                    </x-responsive-nav-link>
+                @endif
+                <x-responsive-nav-link :href="route('sessions.index')" :active="request()->routeIs('sessions.*')">
+                    المحاضرات
+                </x-responsive-nav-link>
             </div>
 
-            <div class="mt-3 space-y-1">
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <button type="submit" class="block w-full text-right pr-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">
-                        تسجيل الخروج
-                    </button>
-                </form>
+            <!-- Responsive Settings Options -->
+            <div class="pt-4 pb-1 border-t border-gray-200">
+                <div class="px-4">
+                    <div class="font-medium text-base text-gray-800">{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}</div>
+                    <div class="font-medium text-sm text-gray-500">{{ auth()->user()->email }}</div>
+                </div>
+
+                <div class="mt-3 space-y-1">
+                    <x-responsive-nav-link :href="route('profile')">
+                        الملف الشخصي
+                    </x-responsive-nav-link>
+
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <x-responsive-nav-link :href="route('logout')"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            تسجيل الخروج
+                        </x-responsive-nav-link>
+                    </form>
+                </div>
             </div>
-        </div>
+        @endauth
     </div>
 </nav>
 
